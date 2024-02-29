4c524141d43b092aa32280cb2a84544542cf6353
Urvi Patel <urvipatel922@gmail.com>
2024-02-29 10:53:21 -0500

    First commit of NodeJS project

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'nodejs',
+    password: 'MYSQLl0g!n'
+})
+
+module.exports = pool.promise();
