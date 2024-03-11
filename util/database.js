50d78cbe6b574a48308f1a743fcea563acf7ff12
Urvi Patel <urvipatel922@gmail.com>
2024-03-11 14:01:36 -0400

    git ignore updat4e

--- util/database.js
+++ util/database.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2');
 
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'nodejs',
-    password: 'MYSQLl0g!n'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABSE,
+    password: process.env.DB_PASSWORD
 })
 
-module.exports = pool.promise();
 
+module.exports = pool.promise();
