d832347bdae9962fce815c67972e6d06f368a7dd
Urvi Patel <urvipatel922@gmail.com>
2024-03-18 13:40:34 -0400

    CHanges to deploy

--- util/database.js
+++ util/database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABSE,
-    password: process.env.DB_PASSWORD
+    host: 'localhost',
+    user: 'root',
+    database: 'nodejs',
+    password: 'MYSQLl0g!n'
 })
 
 
